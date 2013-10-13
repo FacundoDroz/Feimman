2c47aff973d641ba883f70ab9b0c8bdb52545a79
Facundo Droz <facundodroz@gmail.com>
2013-10-13 15:39:56 -0300

    configuracion twitter

--- public/config/twconfig.php
+++ public/config/twconfig.php
@@ -1,7 +1,7 @@
 <?php
 
-define('YOUR_CONSUMER_KEY', 'Twitter Key');
-define('YOUR_CONSUMER_SECRET', 'Twitter Secret Key');
+define('YOUR_CONSUMER_KEY', '5tMS0qXztuQW0lRBMZCnw');
+define('YOUR_CONSUMER_SECRET', 't8at3ovJn7ZGTl9pxXu7z02nyWx5G44rujROGHdnVpQ');
 
 
 
