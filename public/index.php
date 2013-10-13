38a2f610686ac73130c32767370bda6744a92aba
Facundo Droz <facundodroz@gmail.com>
2013-10-13 16:46:07 -0300

    index con boton

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML SYSTEM>
 <?php
 //Always place this code at the top of the Page
 session_start();
@@ -15,29 +16,45 @@ if (array_key_exists("login", $_GET)) {
     }
 }
 ?>
-<title>onlinewebapplication.com Facebook | Twitter Login</title>
-<style type="text/css">
-    #buttons
-	{
-	text-align:center
-	}
-    #buttons img,
-    #buttons a img
-    { border: none;}
-	h1
-	{
-	font-family:Arial, Helvetica, sans-serif;
-	color:#999999;
-	}
-	
-</style>
+    <head>
+        <title>Feimman</title>
+        <meta charset="utf-8">
+        <link href="styles/index.css" type="text/css" rel="stylesheet">
+    </head>
 
-
-
-<div id="buttons">
+    <body>
+        <div id="header">
+            
+            <div id="apptitle"> FEIMMAN </div>
+            <div id="buttons">
 <h1>Twitter Facebook Login </h1>
     <a href="?login&oauth_provider=twitter"><img src="images/tw_login.png"></a>&nbsp;&nbsp;&nbsp;
-    <a href="?login&oauth_provider=facebook"><img src="images/fb_login.png"></a> <br />
-	<br />
-	<a href="http://onlinewebapplication.com/">http://onlinewebapplication.com</a>   
+   
+    <br />
+   
 </div>
+        </div>
+        <!-- End of header -->
+        
+        <div id="pcontent">
+            
+            <h1>Ingrese Palabra</h1>
+            En caso de ser incorrecta, debajo aparecerá una recomendación<br>
+            
+            <div id="false_content">
+                <form method="post" action="php/process.php">
+                    <input type="text" id="false_word" name="word">
+                    <input type="submit" value="Corroborar" id="submit">
+                </form>
+            </div>
+            <!-- End of false_content -->
+            
+            <div id="true_content">
+                <input type="text" id="true_word">
+            </div>
+            <!-- End of false_content -->
+            
+        </div>
+        <!-- End of pcontent -->
+    </body>
+</html>
