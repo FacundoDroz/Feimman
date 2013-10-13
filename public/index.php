f86f2e8a1ee082eb7c41d4713c24d105b394f42a
Facundo Droz <facundodroz@gmail.com>
2013-10-13 17:03:21 -0300

    no message

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 session_start();
 if (isset($_SESSION['id'])) {
     // Redirection to login page twitter or facebook
-    header("location: index.php");//home.php
+    header("location: home.php");//home.php
 }
 
 if (array_key_exists("login", $_GET)) {
