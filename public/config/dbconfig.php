7d1e40637492a70050427737db31d7cca8df2267
Facundo Droz <facundodroz@gmail.com>
2013-10-13 15:51:55 -0300

    config twitter

--- public/config/dbconfig.php
+++ public/config/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'us-cdbr-east-04.cleardb.com');
 define('DB_USERNAME', 'b392626e679c8d');
 define('DB_PASSWORD', '6412e2c1');
 define('DB_DATABASE', 'heroku_01cc4159cc2dbc7');
