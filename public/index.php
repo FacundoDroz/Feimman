99cd5955a677bde506bf1b3814df058c26f3056e
Facundo Droz <facundodroz@gmail.com>
2013-10-13 15:14:07 -0300

    twitter

--- public/index.php
+++ public/index.php
@@ -1,37 +1,43 @@
-<!DOCTYPE HTML SYSTEM>
-    <head>
-        <title>Feimman</title>
-        <meta charset="utf-8">
-        <link href="/styles/index.css" type="text/css" rel="stylesheet">
-    </head>
+<?php
+//Always place this code at the top of the Page
+session_start();
+if (isset($_SESSION['id'])) {
+    // Redirection to login page twitter or facebook
+    header("location: home.php");
+}
 
-    <body>
-        <div id="header">
-            
-            <div id="apptitle"> FEIMMAN </div>
-            
-        </div>
-        <!-- End of header -->
-        
-        <div id="pcontent">
-            
-            <h1>Ingrese Palabra</h1>
-            En caso de ser incorrecta, debajo aparecerá una recomendación<br>
-            
-            <div id="false_content">
-                <form method="post" action="php/process.php">
-                    <input type="text" id="false_word" name="word">
-                    <input type="submit" value="Corroborar" id="submit">
-                </form>
-            </div>
-            <!-- End of false_content -->
-            
-            <div id="true_content">
-                <input type="text" id="true_word">
-            </div>
-            <!-- End of false_content -->
-            
-        </div>
-        <!-- End of pcontent -->
-    </body>
-</html>
+if (array_key_exists("login", $_GET)) {
+    $oauth_provider = $_GET['oauth_provider'];
+    if ($oauth_provider == 'twitter') {
+        header("Location: login-twitter.php");
+    } else if ($oauth_provider == 'facebook') {
+        header("Location: login-facebook.php");
+    }
+}
+?>
+<title>onlinewebapplication.com Facebook | Twitter Login</title>
+<style type="text/css">
+    #buttons
+	{
+	text-align:center
+	}
+    #buttons img,
+    #buttons a img
+    { border: none;}
+	h1
+	{
+	font-family:Arial, Helvetica, sans-serif;
+	color:#999999;
+	}
+	
+</style>
+
+
+
+<div id="buttons">
+<h1>Twitter Facebook Login </h1>
+    <a href="?login&oauth_provider=twitter"><img src="images/tw_login.png"></a>&nbsp;&nbsp;&nbsp;
+    <a href="?login&oauth_provider=facebook"><img src="images/fb_login.png"></a> <br />
+	<br />
+	<a href="http://onlinewebapplication.com/">http://onlinewebapplication.com</a>   
+</div>
